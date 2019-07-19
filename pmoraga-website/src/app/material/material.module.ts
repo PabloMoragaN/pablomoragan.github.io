4ae4c5f1bc12060b8bd3e73dd0c9b273415c545b
Pablo Moraga <pmoraga@zennio.com>
2019-07-19 13:30:02 +0200

    first modules added

---
+++ pmoraga-website/src/app/material/material.module.ts
@@ -0,0 +1,73 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as Material from "@angular/material";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    Material.MatToolbarModule,
+    Material.MatGridListModule,
+    Material.MatFormFieldModule,
+    Material.MatInputModule,
+    Material.MatRadioModule,
+    Material.MatSelectModule,
+    Material.MatCheckboxModule,
+    Material.MatDatepickerModule,
+    Material.MatNativeDateModule,
+    Material.MatButtonModule,
+    Material.MatSnackBarModule,
+    Material.MatTableModule,
+    Material.MatIconModule,
+    Material.MatPaginatorModule,
+    Material.MatSortModule,
+    Material.MatDialogModule,
+    Material.MatCardModule,
+    Material.MatTabsModule,
+    Material.MatExpansionModule,
+    Material.MatListModule,
+    Material.MatSidenavModule,
+    Material.MatTreeModule,
+    Material.MatProgressBarModule,
+    Material.MatTreeModule,
+    Material.MatProgressSpinnerModule,
+    Material.MatToolbarModule
+    
+
+  ],
+  exports: [
+    Material.MatToolbarModule,
+    Material.MatGridListModule,
+    Material.MatFormFieldModule,
+    Material.MatInputModule,
+    Material.MatRadioModule,
+    Material.MatSelectModule,
+    Material.MatCheckboxModule,
+    Material.MatDatepickerModule,
+    Material.MatNativeDateModule,
+    Material.MatButtonModule,
+    Material.MatSnackBarModule,
+    Material.MatTableModule,
+    Material.MatIconModule,
+    Material.MatPaginatorModule,
+    Material.MatSortModule,
+    Material.MatDialogModule,
+    Material.MatCardModule,
+    Material.MatTabsModule,
+    Material.MatExpansionModule,
+    Material.MatListModule,
+    Material.MatSidenavModule,
+    Material.MatTreeModule,
+    Material.MatProgressBarModule,
+    Material.MatTreeModule,
+    Material.MatProgressSpinnerModule,
+    Material.MatToolbarModule,
+    Material.MatSnackBarModule,
+    Material.MatSlideToggleModule
+ 
+
+
+
+  ],
+  declarations: []
+})
+export class MaterialModule { }
