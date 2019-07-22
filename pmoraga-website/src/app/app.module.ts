13cc384e800069b4d9c38a77573e866251c1d4d4
Pablo Moraga <pmoraga@zennio.com>
2019-07-22 13:26:37 +0200

    home page

--- pmoraga-website/src/app/app.module.ts
+++ pmoraga-website/src/app/app.module.ts
@@ -8,17 +8,30 @@ import { AppComponent } from './app.component';
 import { NavbarAppComponent } from './components/shared/navbar/navbar-app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
+import { AboutComponent } from './components/about/about.component';
+import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarAppComponent,
     HomeComponent,
-    ErrorComponent
+    ErrorComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pmoraga-website/src/app/components/home/home.component.ts
+++ pmoraga-website/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public _sendEmailForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private _formBuilder: FormBuilder) { }
+
+  async ngOnInit() {
+
+
+   await this.createForm();
+
   }
 
+
+  async createForm(){
+    this._sendEmailForm = this._formBuilder.group({
+      name: ['',[Validators.required]],
+      email: ["", [Validators.required]],
+      comment: ["", [Validators.required]],
+  });
+  }
+
+
+  async submitEmail(){
+    console.log(this._sendEmailForm.value);
+  }
 }
