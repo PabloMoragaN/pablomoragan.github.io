8029acc265a9d5d7966524fc5983afc88a0650f1
Pablo Moraga <pmoraga@zennio.com>
2019-07-26 13:26:32 +0200

    minimal changes masthead - browsers compatibility checked

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
+import { animate, transition, state, style, trigger } from '@angular/animations';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  providers: [SpotifyService]
+  providers: [SpotifyService],
+  animations:[trigger('fadeInOut', [
+    state('void', style({
+      opacity: 0
+    })),
+    transition('void <=> *', animate(1000)),
+  ])]
 })
 export class AboutComponent implements OnInit {
 
   public playlists;
   public isReady=false;
+  currentState = 'initial';
+
 
   constructor(private _spotifyService: SpotifyService) { }
 
   spinnerValue(value) {
     return value + "%";
+  }
+
+  changeState() {
+    this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
   }
 
   async ngOnInit() {
